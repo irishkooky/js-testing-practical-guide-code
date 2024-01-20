b11d5df08728f31f368c486b3f32b576566483a4
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2024-01-20 18:37:27 +0900

    Fix addition logic in math.js and add unit tests

---
+++ code/basic-testing-frontend/src/math.test.js
@@ -0,0 +1,53 @@
+import { expect, it } from 'vitest';
+import { add } from './math';
+
+it('should summarize all number value in an array', () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+  // Act
+  const result = add(numbers);
+  // Arrange
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if a least one invalid number is provided', () => {
+  // Arrange
+  const inputs = ['invalid', 1];
+  // Act
+  const result = add(inputs);
+  // Arrange
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  // Arrange
+  const numbers = ['1', '2'];
+  // Act
+  const result = add(numbers);
+  // Arrange
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield 0 if an empty array is provided', () => {
+  // Given
+  const numbers = [];
+  // When
+  const result = add(numbers);
+  // Then
+  expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into the function', () => {
+  const resultFn = () => {
+    add();
+  };
+  expect(resultFn).toThrow();
+});
