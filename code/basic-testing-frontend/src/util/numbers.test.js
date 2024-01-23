331e9c5100c0f8658e2023c58a26d7f9a1a99c71
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2024-01-23 20:53:49 +0900

    Refactor cleanNumbers() test to use toStrictEqual()

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -28,7 +28,7 @@ describe('cleanNumbers()', () => {
 
     const cleanedNumbers = cleanNumbers(numberValues);
 
-    expect(cleanedNumbers[0]).toBeTypeOf('number');
+    expect(cleanedNumbers).toStrictEqual([1, 2]);
   });
 
   it('should throw an error if an array with at least one empty string is provided', () => {
