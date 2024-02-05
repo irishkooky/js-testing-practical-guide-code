87753650da9dadfc8f7374fa2d55db452eb97b2b
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2024-02-05 23:17:59 +0900

    Add email-related tests to hooks.test.js

--- code/06-using-testing-hooks/hooks/hooks.test.js
+++ code/06-using-testing-hooks/hooks/hooks.test.js
@@ -21,6 +21,7 @@ afterAll(() => {
   console.log('afterAll()');
 });
 
+// メインの機能が機能しているか
 it('should update the email', () => {
   const newTestEmail = 'test2@test.com';
 
@@ -29,14 +30,17 @@ it('should update the email', () => {
   expect(user.email).toBe(newTestEmail);
 });
 
+// メインの機能が機能しなかった場合ちゃんと機能していないか
 it('should have an email property', () => {
   expect(user).toHaveProperty('email');
 });
 
+// 初期値の設定大丈夫か
 it('should store the provided email value', () => {
   expect(user.email).toBe(testEmail);
 });
 
+// サブの機能がきのうするかどうか
 it('should clear the email', () => {
   user.clearEmail();
 
