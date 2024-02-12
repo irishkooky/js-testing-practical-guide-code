af5ddabf7de5f786296e53a8d404bc0ec9a701ae
yoshihirokurosaki <yoshihiro.kurosaki.15@gmail.com>
2024-02-12 23:43:39 +0900

    Add import statement for Window class in dom.test.js

--- code/02-setting-up-a-virtual-html-page/util/dom.test.js
+++ code/02-setting-up-a-virtual-html-page/util/dom.test.js
@@ -2,10 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 import { it, vi } from 'vitest';
+// happy-domやjsdomはdomをサポートしているただの環境。
 import { Window } from 'happy-dom';
 
 import { showError } from './dom';
-
+// cwd = current working directory
 const htmlDocPath = path.join(process.cwd(), 'index.html');
 const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
@@ -16,4 +17,4 @@ vi.stubGlobal('document', document);
 
 it('first test', () => {
   showError('test');
-})
+});
